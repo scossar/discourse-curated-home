71ca225842611f1b1c4fa01bc61abbc1ff77d0d0
scossar <scossar3@gmail.com>
2015-11-10 19:25:06 -0800

    Add anonymous filter

--- plugin.rb
+++ plugin.rb
@@ -4,13 +4,15 @@
 # authors: Benjamin Kampmann
 
 
-register_asset "javascripts/curated_home.js"
 register_asset "curated_home_style.scss"
 
 after_initialize do
-    require_dependency File.expand_path('../integrate.rb', __FILE__)
+  require_dependency File.expand_path('../integrate.rb', __FILE__)
 end
 
 
 # Odd, but we have to register first, otherwise routes won't pick it up
 Discourse.filters << "curated"
+
+# Allow anonymous access
+Discourse.anonymous_filters << "curated"
